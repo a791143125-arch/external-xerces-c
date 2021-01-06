4a37eb417087bb4d8121057246ccf0f3b6d3bcbb
Julien Wadel <julien.wadel@belledonne-communications.com>
2021-01-06 10:41:19 +0100

    UWP support

--- config.h.cmake
+++ config.h.cmake
@@ -509,3 +509,5 @@
 /* Define to empty if the keyword `volatile' does not work. Warning: valid
    code using `volatile' can become incorrect without. Disable with care. */
 #cmakedefine volatile
+
+#cmakedefine XERCES_WINDOWS_UWP 1
